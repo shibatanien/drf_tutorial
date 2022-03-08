ac16b4a5f657ef5301ce9da98de70fb0b4ddd826
Koki Shibata <koki.shibata@cloud-consulting.asia>
2022-03-08 12:48:16 +0800

    naming urls

--- drinks/urls.py
+++ drinks/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('drinks/', views.DrinkList.as_view()),
-    path('drinks/<int:pk>/', views.DrinkDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view())
+    path('drinks/', views.DrinkList.as_view(), name='drink_list'),
+    path('drinks/<int:pk>/', views.DrinkDetail.as_view(), name='drink_detail'),
+    path('users/', views.UserList.as_view(), name='user_list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail')
 ]
 
 urlpatterns += [
